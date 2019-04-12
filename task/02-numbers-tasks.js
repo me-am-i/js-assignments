503e1c16790769cca9ab920917244b0583405981
dgm <vsl_sv@bk.ru>
2019-04-13 00:47:12 +0300

    feat: three passed tests 02

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -54,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    console.log(value1, value2);
+    if (value1 == value2) return value1;
     return (value1 + value2)/2;
 }
 
